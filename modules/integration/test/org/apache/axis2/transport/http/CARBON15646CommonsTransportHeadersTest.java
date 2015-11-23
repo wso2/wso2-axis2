912aa5ef6b67e23f16aa49d24538405212b4282a
Manuri <amaya.uom@gmail.com>
2015-11-23 09:49:26 +0530

    CARBON-15646: Adding license header and refactoring

--- modules/integration/test/org/apache/axis2/transport/http/CARBON15646CommonsTransportHeadersTest.java
+++ modules/integration/test/org/apache/axis2/transport/http/CARBON15646CommonsTransportHeadersTest.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.axis2.transport.http;
 
 import junit.framework.TestCase;
@@ -12,7 +31,7 @@ public class CARBON15646CommonsTransportHeadersTest extends TestCase {
     CommonsTransportHeaders commonsTransportHeaders;
     String value;
 
-    public void initForRandomNoOfHeaders() {
+    public void initHeaders() {
         int numberOfHeaders = ((Double) (Math.random() * 10000 % 100)).intValue() + 10;
 
         headers = new Header[numberOfHeaders];
@@ -28,10 +47,8 @@ public class CARBON15646CommonsTransportHeadersTest extends TestCase {
     }
 
     @Test
-    public void testForCARBON15646() {
-
-        initForRandomNoOfHeaders();
-
+    public void testCompleteHeaderValueInclusion() {
+        initHeaders();
         // This is to get init() method called.
         commonsTransportHeaders.isEmpty();
         HashMap headerMap = commonsTransportHeaders.headerMap;
