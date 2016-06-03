8fc8935de47e328e268d2a0820c2fadea5b8efe4
Kalpa Welivitigoda <callkalpa@gmail.com>
2016-06-03 18:44:46 +0530

    Revert "Fixing the ESBJAVA-4014 issue"

--- modules/kernel/src/org/apache/axis2/util/Utils.java
+++ modules/kernel/src/org/apache/axis2/util/Utils.java
@@ -75,9 +75,6 @@ import java.net.InetAddress;
 
 public class Utils {
     private static final Log log = LogFactory.getLog(Utils.class);
-    private static final String LOCAL_TRANSPORT_PREFIX = "local:/";
-    private static final String HTTP_URL_PREFIX = "http://";
-    private static final String AXIS2_SERVICE_TEST_PREFIX = "/axis2/services/";
 
     public static void addHandler(Flow flow, Handler handler, String phaseName) {
         HandlerDescription handlerDesc = new HandlerDescription(handler.getName());
@@ -272,22 +269,10 @@ public class Utils {
         	servicePath = servicePath+"/";
         }
 
-        //Adding "/" at the beginning of the "servicePath" to check the "paths" which are starting with "servicePath"
-        if(!servicePath.startsWith("/")){
-            servicePath = "/"+servicePath;
-        }
-
+        int index = path.lastIndexOf(servicePath);
         String serviceOpPart = null;
 
-        /* Here we had to check multiple cases to prevent the tests from failing with the fix given to
-        https://wso2.org/jira/browse/ESBJAVA-4014.
-        1. Accessing admin services through local transport (local:/AuthenticationAdmin)
-        2. Dispatching requests coming to "http://127.0.0.1" kind of paths
-        3. Dispatching requests coming to "10.100.0.37/axis2/services" kind of paths
-         */
-        if (path.startsWith(servicePath) || path.startsWith(LOCAL_TRANSPORT_PREFIX) || path.startsWith(HTTP_URL_PREFIX)
-                || path.contains(AXIS2_SERVICE_TEST_PREFIX)) {
-            int index = path.lastIndexOf(servicePath);
+        if (-1 != index) {
             int serviceStart = index + servicePath.length();
 
             //get the string after services path
